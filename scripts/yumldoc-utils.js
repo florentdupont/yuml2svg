bdf3b7d517282f2f54a62c7bd14fb5d1f212db58
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2017-11-22 09:41:09 +0100

    Get rid of declarations in the global scope

--- scripts/yumldoc-utils.js
+++ scripts/yumldoc-utils.js
@@ -1,140 +1,131 @@
-const fs = require('fs');
-const classDiagram = require('./class-diagram.js');
-const usecaseDiagram = require('./usecase-diagram.js');
-const activityDiagram = require('./activity-diagram.js');
-const stateDiagram = require('./state-diagram.js');
-const deploymentDiagram = require('./deployment-diagram.js');
-const packageDiagram = require('./package-diagram.js')
+const fs = require("fs");
+const classDiagram = require("./class-diagram.js");
+const usecaseDiagram = require("./usecase-diagram.js");
+const activityDiagram = require("./activity-diagram.js");
+const stateDiagram = require("./state-diagram.js");
+const deploymentDiagram = require("./deployment-diagram.js");
+const packageDiagram = require("./package-diagram.js");
 const Viz = require("viz.js");
-require('./svg-utils.js')();
+const processEmbeddedImages = require("./svg-utils.js");
 
-module.exports = function()
-{
-    this.processYumlDocument = function(text, filename, mayGenerate) 
-    {
-        var newlines = [];
-        var options = { dir: "TB", generate: false };
+const processYumlDocument = function(text, filename, mayGenerate) {
+  var newlines = [];
+  var options = { dir: "TB", generate: false };
 
-        var lines = text.split(/\r|\n/);
+  var lines = text.split(/\r|\n/);
 
-        for (var i=0; i<lines.length; i++)
-        {
-            var line = lines[i].replace(/^\s+|\s+$/g,'');  // Removes leading and trailing spaces
-            if (line.startsWith("//"))
-                processDirectives(line, options);
-            else if (line.length > 0)
-                newlines.push(line);
-        }
+  for (var i = 0; i < lines.length; i++) {
+    var line = lines[i].replace(/^\s+|\s+$/g, ""); // Removes leading and trailing spaces
+    if (line.startsWith("//")) processDirectives(line, options);
+    else if (line.length > 0) newlines.push(line);
+  }
 
-        if (newlines.length == 0)
-            return "";
+  if (newlines.length == 0) return "";
 
-        if (!options.hasOwnProperty("type"))
-        {
-            options.error = "Error: Missing mandatory 'type' directive";
-        }
+  if (!options.hasOwnProperty("type")) {
+    options.error = "Error: Missing mandatory 'type' directive";
+  }
 
-        if (options.hasOwnProperty("error"))
-        {
-            return options.error;
-        }
+  if (options.hasOwnProperty("error")) {
+    return options.error;
+  }
 
-        var dot = null;
+  var dot = null;
 
-        try {
-            switch (options.type)
-            {
-                case "class":
-                    dot = classDiagram(newlines, options);
-                    break;
-                case "usecase":
-                    dot = usecaseDiagram(newlines, options);
-                    break;
-                case "activity":
-                    dot = activityDiagram(newlines, options);
-                    break;
-                case "state":
-                    dot = stateDiagram(newlines, options);
-                    break;
-                case "deployment":
-                    dot = deploymentDiagram(newlines, options);
-                    break;
-                case "package":
-                    dot = packageDiagram(newlines, options);
-                    break;
-            }
-        }
-        catch (e) {
-            return "Error parsing the yUML file";
-        }
-
-        if (dot == null)
-            return "Error: unable to parse the yUML file";
-
-        var svgLight, svgDark;
-        try {
-            svgLight = Viz(buildDotHeader(false) + dot);
-            svgLight = processEmbeddedImages(svgLight, false);
-
-            svgDark = Viz(buildDotHeader(true) + dot);
-            svgDark = processEmbeddedImages(svgDark, true);
-        }
-        catch (e) {
-            return "Error composing the diagram"
-        }
-
-        try {
-            if (options.generate===true && mayGenerate===true)
-            {
-                var imagename = filename.replace(/\.[^.$]+$/, '.svg');
-                fs.writeFileSync(imagename, svgLight);
-            }                            
-        }
-        catch (e) { }
-
-        return "<div id='light'>\r\n" + svgLight + "\r\n</div><div id='dark'>\r\n" + svgDark + "\r\n</div>";
+  try {
+    switch (options.type) {
+      case "class":
+        dot = classDiagram(newlines, options);
+        break;
+      case "usecase":
+        dot = usecaseDiagram(newlines, options);
+        break;
+      case "activity":
+        dot = activityDiagram(newlines, options);
+        break;
+      case "state":
+        dot = stateDiagram(newlines, options);
+        break;
+      case "deployment":
+        dot = deploymentDiagram(newlines, options);
+        break;
+      case "package":
+        dot = packageDiagram(newlines, options);
+        break;
     }
+  } catch (e) {
+    return "Error parsing the yUML file";
+  }
 
-    processDirectives = function(line, options)
-    {
-        const directions = {
-            leftToRight: "LR",
-            rightToLeft: "RL",
-            topDown: "TB"
-        };
+  if (dot == null) return "Error: unable to parse the yUML file";
 
-        var keyvalue = /^\/\/\s+\{\s*([\w]+)\s*:\s*([\w]+)\s*\}$/.exec(line);  // extracts directives as:  // {key:value}
-        if (keyvalue != null && keyvalue.length == 3)
-        {
-            var key = keyvalue[1];
-            var value = keyvalue[2];
+  var svgLight, svgDark;
+  try {
+    svgLight = Viz(buildDotHeader(false) + dot);
+    svgLight = processEmbeddedImages(svgLight, false);
 
-            switch (key)
-            {
-                case "type":
-                    if (/^(class|usecase|activity|state|deployment|package)$/.test(value))
-                        options.type = value;
-                    else {
-                        options.error = "Error: invalid value for 'type'. Allowed values are: class, usecase, activity, state, deployment, package.";
-                        return;
-                    }
-                    break;
-                case "direction":
-                    if (/^(leftToRight|rightToLeft|topDown)$/.test(value))
-                        options.dir = directions[value];
-                    else {
-                        options.error = "Error: invalid value for 'direction'. Allowed values are: leftToRight, rightToLeft, topDown <i>(default)</i>.";
-                        return;
-                    }
-                    break;
-                case "generate":
-                    if (/^(true|false)$/.test(value))
-                        options.generate = (value === "true");
-                    else {
-                        options.error = "Error: invalid value for 'generate'. Allowed values are: true, false <i>(default)</i>.";
-                        return;
-                    }
-            }
+    svgDark = Viz(buildDotHeader(true) + dot);
+    svgDark = processEmbeddedImages(svgDark, true);
+  } catch (e) {
+    return "Error composing the diagram";
+  }
+
+  try {
+    if (options.generate === true && mayGenerate === true) {
+      var imagename = filename.replace(/\.[^.$]+$/, ".svg");
+      fs.writeFileSync(imagename, svgLight);
+    }
+  } catch (e) {}
+
+  return (
+    "<div id='light'>\r\n" +
+    svgLight +
+    "\r\n</div><div id='dark'>\r\n" +
+    svgDark +
+    "\r\n</div>"
+  );
+};
+
+processDirectives = function(line, options) {
+  const directions = {
+    leftToRight: "LR",
+    rightToLeft: "RL",
+    topDown: "TB",
+  };
+
+  var keyvalue = /^\/\/\s+\{\s*([\w]+)\s*:\s*([\w]+)\s*\}$/.exec(line); // extracts directives as:  // {key:value}
+  if (keyvalue != null && keyvalue.length == 3) {
+    var key = keyvalue[1];
+    var value = keyvalue[2];
+
+    switch (key) {
+      case "type":
+        if (/^(class|usecase|activity|state|deployment|package)$/.test(value))
+          options.type = value;
+        else {
+          options.error =
+            "Error: invalid value for 'type'. Allowed values are: class, usecase, activity, state, deployment, package.";
+          return;
+        }
+        break;
+      case "direction":
+        if (/^(leftToRight|rightToLeft|topDown)$/.test(value))
+          options.dir = directions[value];
+        else {
+          options.error =
+            "Error: invalid value for 'direction'. Allowed values are: leftToRight, rightToLeft, topDown <i>(default)</i>.";
+          return;
+        }
+        break;
+      case "generate":
+        if (/^(true|false)$/.test(value)) options.generate = value === "true";
+        else {
+          options.error =
+            "Error: invalid value for 'generate'. Allowed values are: true, false <i>(default)</i>.";
+          return;
         }
     }
-}
+  }
+};
+
+module.exports = processYumlDocument;
