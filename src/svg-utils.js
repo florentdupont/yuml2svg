16f802bb7ef492cbaac389383655209589630fde
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-04-27 16:09:33 +0200

    Optimize svg-util module

--- src/svg-utils.js
+++ src/svg-utils.js
@@ -1,49 +1,59 @@
 const shapes = {
-  actor: [
-    `<circle cx="0" cy="-20" r="7.5" />
-                <line x1="0" y1="-12.5" x2="0" y2="5" />
-                <line x1="-15" y1="-5" x2="15" y2="-5" />
-                <line x1="0" y1="5" x2="-15" y2="17" />
-                <line x1="0" y1="5" x2="15" y2="17" />`,
-    0,
-    25,
-  ],
+  actor: {
+    svgNodes: [
+      '<circle cx="0" cy="-20" r="7.5" />',
+      '<line x1="0" y1="-12.5" x2="0" y2="5" />',
+      '<line x1="-15" y1="-5" x2="15" y2="-5" />',
+      '<line x1="0" y1="5" x2="-15" y2="17" />',
+      '<line x1="0" y1="5" x2="15" y2="17" />',
+    ],
+    translateX: 0,
+    translateY: 25,
+  },
 };
 
 /**
+ * Process embedded shapes {img:shapeName}
  * @param {string} svg
  * @param {boolean} isDark
  * @returns {string}
  */
 module.exports = function(svg, isDark) {
-  const expr = /<text\s.*>{img:.*}.*<\/text>/g;
+  const expr = /<text\s(.*)>{img:(.*)}(.*)<\/text>/g;
 
-  svg = svg.replace(expr, function(match) {
+  return svg.replace(expr, function(match, attributes, shapeName, label) {
     try {
-      const parts = /<text\s(.*)>{img:(.*)}(.*)<\/text>/.exec(match);
-      const text = "<text " + parts[1] + ">" + parts[3].trim() + "</text>";
+      const textNode = `<text ${attributes}>${label.trim()}</text>`;
 
-      if (!shapes.hasOwnProperty(parts[2])) return text;
+      if (shapeName in shapes) {
+        const img = shapes[shapeName];
+        const [
+          _,
+          translateX,
+          translateY,
+        ] = /<text\s.*x=\"(-?[0-9\.]+)\" y=\"(-?[0-9\.]+)\"/.exec(textNode);
 
-      const translate = /<text\s.*x=\"(-?[0-9\.]+)\" y=\"(-?[0-9\.]+)\"/.exec(
-        text
-      );
-      const [_, x, y] = translate;
-
-      const img = shapes[parts[2]];
-
-      return (
-        `<g transform="translate(${x}, ${y})" style="fill:none;stroke:${
-          isDark ? "white" : "black"
-        };stroke-width:1px">${img[0]}</g>\n` +
-        text
-          .replace(' x="' + x + '"', ' x="' + (parseFloat(x) + img[1]) + '"')
-          .replace(' y="' + y + '"', ' y="' + (parseFloat(y) + img[2]) + '"')
-      );
+        return (
+          `<g transform="translate(${translateX}, ${translateY})" style="fill:none;stroke:${
+            isDark ? "white" : "black"
+          };stroke-width:1px">${img.svgNode.join("")}</g>\n` +
+          textNode
+            .replace(
+              ' x="' + x + '"',
+              ' x="' + (parseFloat(translateX) + img.translateX) + '"'
+            )
+            .replace(
+              ' y="' + y + '"',
+              ' y="' + (parseFloat(translateY) + img.translateY) + '"'
+            )
+        );
+      } else {
+        console.warn(new Error(`Unknown shape '${shapeName}'`));
+        return textNode;
+      }
     } catch (e) {
+      console.warn(e);
       return match;
     }
   });
-
-  return svg;
 };
