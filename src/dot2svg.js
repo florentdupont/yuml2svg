0226242c43aba71434e2e15ced640f77c94238fd
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-05-01 00:38:33 +0200

    Fix error on Node

--- src/dot2svg.js
+++ src/dot2svg.js
@@ -3,7 +3,7 @@ const Viz = require("viz.js");
 if (typeof IS_BROWSER === "undefined") IS_BROWSER = false;
 
 if (!IS_BROWSER) {
-  const { Module, render } = require("viz.js/full.js.opaque");
+  var { Module, render } = require("viz.js/full.js.opaque");
 }
 
 module.exports = (dot, vizOptions) => {
