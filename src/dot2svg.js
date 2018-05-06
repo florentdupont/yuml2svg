d786fa3f096f6c2605c84c36a9010ed9ec13d376
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-05-07 01:36:13 +0200

    Fix warning when generating several Viz instances

--- src/dot2svg.js
+++ src/dot2svg.js
@@ -4,11 +4,10 @@ const Viz = require("viz.js");
 
 if (typeof IS_BROWSER === "undefined" || !IS_BROWSER) {
   var { Module, render } = require("viz.js/full.js.opaque");
+  var viz = new Viz({ Module, render });
 }
 
 module.exports = (dot, vizOptions) => {
-  if (!vizOptions) {
-    vizOptions = { Module, render };
-  }
-  return new Viz(vizOptions).renderString(dot);
+  const rendered = vizOptions ? new Viz(vizOptions) : viz;
+  return viz.renderString(dot);
 };
