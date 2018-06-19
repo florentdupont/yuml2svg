4e63c4748ed4333bacf01ac04693442e4430b9b8
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-06-20 00:19:08 +0200

    Remove usage of ES2018 to support node v6

--- src/wrapDotDocument.js
+++ src/wrapDotDocument.js
@@ -30,12 +30,16 @@ const DARK_HEADER = {
 module.exports = (document, isDark, overrides = {}) =>
   Object.entries(DEFAULT_HEADER).reduce(
     (pv, [type, defaultSettings]) =>
-      `${pv}\n\t${type}[${Object.entries({
-        fontname: DEFAULT_FONT,
-        ...defaultSettings,
-        ...DARK_HEADER[isDark && type],
-        ...overrides[type],
-      })
+      `${pv}\n\t${type}[${Object.entries(
+        Object.assign(
+          {
+            fontname: DEFAULT_FONT,
+          },
+          defaultSettings,
+          DARK_HEADER[isDark && type],
+          overrides[type]
+        )
+      )
         .map(entry => entry.join("="))
         .join(",")}]`,
     "digraph G{"
